4440732910ee8503acf9dd8d8cbdcd579be086da
noor <noormuhammadmalik95@gmail.com>
2021-11-17 12:59:48 +0500

    Finish the sqlite3 handler with basic CRUD.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vocabulary Storage
+
+This is a simple project that keeps track of the vocabulary that you will
+when working with any research project. You can store keywords, look them up,
+as the core functionality.
---
+++ src/db_handler.py
@@ -0,0 +1,114 @@
+# This is the database handler module that handles the database 
+# operations and abstracts the internals of the database
+# from the main applications that needs to perform CRUD operations.
+import sqlite3
+
+class SqliteHandler():
+    # A static variable to store the database file path
+    sqlite_db_path = "../db/vocab.db"
+    sqlite_db_name = sqlite_db_path.split("/")[-1]
+
+    # A DB schema to store the k/v pairs
+    # for now, it's just an index, a phrase and some description
+    sqlite_db_schema = "(phrase TEXT, description TEXT)"
+    
+    # The table name in the database
+    sqlite_db_table_name = "vocab"
+
+    # A constructor to initialize the database for usage upon application 
+    # running.
+    def __init__(self):
+        """
+        This constructor needs to initialize the database handler by
+        opening the file as stored in the static variable. It will also
+        start a cursor on the database to allow for queries execution.
+        """
+        try:
+            self.connection = sqlite3.connect(SqliteHandler.sqlite_db_path)
+        except Exception as e:
+            print("[DEBUG] Exception occured when connecting to the database.")
+            print("Dying\n{}", e)
+            exit(1)
+
+        self.cursor = self.connection.cursor()
+
+        # check if the table already exists in the database
+        get_table_query = "SELECT name FROM sqlite_master WHERE type='table'"
+        self.cursor.execute(get_table_query)
+        tables_tuple = self.cursor.fetchone()
+
+        # If there are no tables in the db, tables_tuple will be None
+        if tables_tuple is not None and SqliteHandler.sqlite_db_table_name in tables_tuple:
+            print("[DEBUG] Table {} already exists in the database {}".format(
+                SqliteHandler.sqlite_db_table_name,
+                SqliteHandler.sqlite_db_name
+            ))
+        else:
+            # generate the table query
+            create_table_query = '''CREATE TABLE vocab ''' + SqliteHandler.sqlite_db_schema
+            # Check if a table exists in the database to store the keywords
+            self.cursor.execute(create_table_query)
+            # Commit the changes
+            self.connection.commit()
+
+        print("[DEBUG] Database ready.")
+
+    # A destructor to clean up database connections, if any are open
+    def __del__(self):
+        # close the connection
+        self.connection.close()
+
+    # A method to retrieve an entry from the database, all matching entries as well
+    def get_from_db(self, keyword):
+        """This method will return entries from the database as requested.
+        Since it will perform pattern matching, it can fetch multiple entries.
+        It accepts a keyword to search."""
+
+        # TODO: Perform sanitization of the keyword here before stuffing
+        # it in the query.
+        get_query = "SELECT * from {} WHERE phrase LIKE '%{}%'".format(
+            SqliteHandler.sqlite_db_table_name,
+            keyword
+        )
+        self.cursor.execute(get_query)
+
+        # return all the rows that were fetched
+        return self.cursor.fetchall()
+
+    # A method for updating an existing phrase in the database
+    def update_in_db(self, phrase, desc):
+        response = self.get_from_db(phrase)
+        if response is not None:
+            # If the phrase exists, add the description as an additional
+            # paragraph to the phrase
+            update_query = "UPDATE {} SET description='{}' WHERE phrase='{}'".format(
+                SqliteHandler.sqlite_db_table_name,
+                response[0][1] + "\n" + desc,
+                phrase
+            )
+            self.cursor.execute(update_query)
+            self.connection.commit()
+        else:
+            # If the phrase doesn't exist, simply add it
+            self.put_in_db(phrase, desc)
+
+    # A method to write a phrase and description to the database
+    def put_in_db(self, phrase, desc):
+        """This method will return success/failure status of a query that
+        has successfully or unsuccessfully been executed on the database
+        for storing data."""
+
+        # TODO: Perform sanitization of the data before stuffing it in the
+        # query here
+
+        # generate the query for inserting the data into the database table
+        put_query = "INSERT INTO {} VALUES ('{}','{}')".format(
+            SqliteHandler.sqlite_db_table_name,
+            phrase,
+            desc
+        )
+        # execute the query to add the data in the database table
+        self.cursor.execute(put_query)
+
+        # commit changes to the database on disk
+        self.connection.commit()
